b2cbf2fcc2a0b2cae40a3c482af1a3bef63c7213
Lachlan Hurst <lachlan.hurst@gmail.com>
2024-04-15 15:01:06 +1000

    Include new Hole and Gap check in FinderGC QAX plugin

--- ausseabed/findergc/lib/allchecks.py
+++ ausseabed/findergc/lib/allchecks.py
@@ -1,7 +1,9 @@
 from .fliersgridcheck import FliersCheck
+from .holeandgapcheck import HoleAndGapCheck
 from .holesgridcheck import HolesCheck
 
 all_checks = [
     FliersCheck,
+    HoleAndGapCheck,
     HolesCheck
 ]
